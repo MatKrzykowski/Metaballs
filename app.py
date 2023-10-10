296a31c72367340a5941186103fb035c6289b8df
MKrzykow <matkrzykowski@gmail.com>
2023-10-10 21:11:04 +0200

    Refactor out calculate_hue_arr

--- app.py
+++ app.py
@@ -14,21 +14,13 @@ class App():
         self.particles = gen_particles(config.n_particles)
         self.memory = init_memory(config.width, config.height, config.n_particles)
         self.renderer = Renderer(config.width, config.height, 18)
+        self.hue_arr = self.memory.hue_arr
 
     def main(self):
         while True:
             self.renderer.start_frame()
 
-            self.memory.pos_part[:, :, :, :] = np.array([[p.x, p.y] for p in self.particles
-                                ]).transpose().reshape(
-                                    (2, 1, 1, config.n_particles))
-
-            np.subtract(self.memory.pos_grid, self.memory.pos_part, out=self.memory.dist_xy)
-            np.reciprocal(
-                np.hypot(self.memory.dist_xy[0], self.memory.dist_xy[1]), out=self.memory.inv_dist)
-            np.sum(self.memory.inv_dist, axis=2, out=self.memory.hue_arr)
-            np.multiply(config.particle_size, self.memory.hue_arr, out=self.memory.hue_arr)
-            np.clip(self.memory.hue_arr, 0, 1, out=self.memory.hue_arr)
+            hue_arr = self.calculate_hue_arr()
 
             for particle in self.particles:
                 particle.timestep()
@@ -38,6 +30,37 @@ class App():
                     return
 
             fps = str(round(self.fpsClock.get_fps(), 1))
-            self.renderer.draw_frame(self.memory.hue_arr, fps)
+            self.renderer.draw_frame(hue_arr, fps)
             pygame.display.update()
             self.fpsClock.tick(config.fps)
+
+    def calculate_hue_arr(self):
+        pos_part_list = [[part.x, part.y] for part in self.particles]
+        self.memory.pos_part[:, :, :, :] = np.array(
+                pos_part_list
+                ).transpose(
+                ).reshape((2, 1, 1, config.n_particles))
+        np.subtract(
+            self.memory.pos_grid,
+            self.memory.pos_part,
+            out=self.memory.dist_xy)
+        np.hypot(
+            self.memory.dist_xy[0],
+            self.memory.dist_xy[1],
+            out=self.memory.inv_dist)
+        np.reciprocal(
+            self.memory.inv_dist,
+            out=self.memory.inv_dist)
+        np.sum(
+            self.memory.inv_dist,
+            axis=2,
+            out=self.hue_arr)
+        np.multiply(
+            config.particle_size,
+            self.hue_arr,
+            out=self.hue_arr)
+        np.clip(
+            self.hue_arr,
+            0, 1,
+            out=self.hue_arr)
+        return self.hue_arr
